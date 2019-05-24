6d63e48231693b600c1afb371e8ceaf5602f9a62
ramtinsoltani <ramtinsoltani93@gmail.com>
2019-05-24 10:08:43 -0700

    Added file extension recognition for displaying different file icons.

--- src/app/components/explorer/explorer.component.ts
+++ src/app/components/explorer/explorer.component.ts
@@ -102,6 +102,127 @@ export class ExplorerComponent implements OnInit, OnDestroy {
 
   }
 
+  public getTypeIcon(filename: string): string {
+
+    let extension: any = filename.match(/\.[^\.]+$/);
+
+    if ( ! extension ) return 'file';
+
+    extension = extension[0].toLowerCase();
+
+    switch ( extension ) {
+
+      case '.txt':
+      case '.md':
+      case '.markdown':
+      case '.rtf':
+      case '.tex':
+      case '.odt':
+        return 'file-alt';
+      case '.doc':
+      case '.docx':
+      case '.docm':
+        return 'file-word';
+      case '.xls':
+      case '.xlsx':
+      case '.xlsm':
+        return 'file-excel';
+      case '.ppt':
+      case '.pps':
+      case '.pptx':
+      case '.ppsx':
+      case '.pptm':
+      case '.ppsm':
+        return 'file-powerpoint';
+      case '.pdf':
+        return 'file-pdf';
+      case '.csv':
+        return 'file-csv';
+      case '.pem':
+      case '.key':
+      case '.p7b':
+      case '.p7c':
+      case '.cer':
+        return 'file-contract';
+      case '.zip':
+      case '.rar':
+      case '.iso':
+      case '.tar':
+      case '.gz':
+      case '.xz':
+      case '.7z':
+      case '.apk':
+      case '.cab':
+      case '.dmg':
+      case '.jar':
+        return 'file-archive';
+      case '.mp4':
+      case '.mkv':
+      case '.mpeg':
+      case '.wmv':
+      case '.webm':
+      case '.flv':
+      case '.vob':
+      case '.ogv':
+      case '.avi':
+      case '.mov':
+      case '.m4v':
+      case '.mpg':
+      case '.3gp':
+        return 'file-video';
+      case '.aac':
+      case '.aiff':
+      case '.amr':
+      case '.flac':
+      case '.m4a':
+      case '.mp3':
+      case '.ogg':
+      case '.wav':
+      case '.wma':
+        return 'file-audio';
+      case '.jpg':
+      case '.jpeg':
+      case '.gif':
+      case '.png':
+      case '.svg':
+      case '.bmp':
+      case '.ico':
+      case '.tiff':
+      case '.icns':
+        return 'file-image';
+      case '.html':
+      case '.htm':
+      case '.js':
+      case '.ts':
+      case '.css':
+      case '.scss':
+      case '.sass':
+      case '.less':
+      case '.json':
+      case '.py':
+      case '.cpp':
+      case '.h':
+      case '.cs':
+      case '.vbs':
+      case '.go':
+      case '.rb':
+      case '.php':
+      case '.java':
+      case '.swift':
+      case '.c':
+      case '.vb':
+      case '.sh':
+      case '.xhtml':
+      case '.jsp':
+      case '.asp':
+        return 'file-code';
+      default:
+        return 'file';
+
+    }
+
+  }
+
   ngOnDestroy() {
 
     if ( this.pathSub && ! this.pathSub.closed ) this.pathSub.unsubscribe();
